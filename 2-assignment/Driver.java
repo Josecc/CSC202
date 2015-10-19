7097d88763c7e53a3e4183bb667790c67ea62197
Jose Canahui <jose.canahui@gmail.com>
2015-10-19 17:30:39 -0400

    Finally found out how to get JavaFX working...

--- 2-assignment/Driver.java
+++ 2-assignment/Driver.java
@@ -1,26 +1,45 @@
 import model.*;
 import java.util.Scanner;
 
-public class Driver {
-	public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+
+public class Driver extends Application{
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Assignmnent 2");
+
+		Group root = new Group();
+		Scene scene = new Scene(root, 600, 500, Color.GRAY);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main (String[] args) {
 		boolean acceptable = false;
 		Scanner keyboard = new Scanner(System.in);
+		int answer = 0;
 		while(!acceptable){
 			System.out.println("Hello! Would you like to run (1) the simpulator or (2) the GUI?");
 			try{
-				int answer = keyboard.nextInt();
+				answer = keyboard.nextInt();
 				if((answer < 1) || (answer > 2))
-					throw new InputMismatchException();
+					throw new Exception();
 				acceptable = true;
-			} catch(InputMismatchException e) {
+			} catch(Exception e) {
 				System.out.println("Please enter either a '1' or a '2'.");
+				keyboard.next();
 			}
 		}
 
 		if(answer == 1){
-			//run simulator CLI
+			
 		} else {
-			//run GUI
+			launch(args);
 		}
 
 		//save stuff?
