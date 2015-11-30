0834282dc3ff54725c533e98aa5aebc73e73ee7e
TheVoilaApp <jose.canahui@gmail.com>
2015-11-30 13:22:34 -0500

    added priority queue

---
+++ 5-assignment/Priority Queue/PriQueueInterface.java
@@ -0,0 +1,19 @@
+public interface PriQueueInterface<T extends Comparable<T>>
+{
+  boolean isEmpty();
+  // Returns true if this priority queue is empty; otherwise, returns false.
+
+  boolean isFull();
+  // Returns true if this priority queue is full; otherwise, returns false.
+
+  void enqueue(T element); 
+  // Precondition: element is Comparable
+  //
+  // Throws PriQOverflowException if this priority queue is full;
+  // otherwise, adds element to this priority queue.
+
+  T dequeue();
+  // Throws PriQUnderflowException if this priority queue is empty;
+  // otherwise, removes element with highest priority from this 
+  // priority queue and returns it.
+}
